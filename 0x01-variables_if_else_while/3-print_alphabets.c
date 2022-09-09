7244744a7d399e783e5aaad6a061b3a7b86b88a4
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-09-09 16:37:46 -0500

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -18,7 +18,8 @@ int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
-if (ch != 'e' && ch != 'q')
+putchar(ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
