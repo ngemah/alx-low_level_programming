0dc86c1d0cf6cc2114e91eda6895cae9d588a781
Richard <richard.cudjoe@aiesec.net>
2022-09-10 14:19:09 -0700

    4. hello puts'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+
+
+/**
+
+ * main - A simple program that outputs a line of text using puts()
+
+ *
+
+ * Return: 0 upon completion
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
