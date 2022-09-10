5fc381afb510e8c0ab6f2b69d4efe4c5b36d0085
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-09-09 20:16:21 -0500

    corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,19 +16,16 @@ int main(void)
 {
 int n;
 int m;
-for (n = 48; n <= 56; n++)
+for (n = 48; n < 57; n++)
 {
-for (m = 49; m <= 57; m++)
+for (m = n + 1; m < 58; m++)
 {
-if (m > n)
-{
-putchar(',');
-putchar(' ');
+putchar(n);
+putchar(m);
 if (n != 56 || m != 57)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
