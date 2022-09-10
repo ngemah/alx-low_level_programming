906592f3bbe1da73e07032bf50966bd73cb28d03
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-09-10 12:39:15 -0500

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+/*
+* File: 101-print_comb4.c
+*
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - printing all possible different combinations of two digits.
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+int m;
+for (n = 48; n < 57; n++)
+{
+for (m = n + 1; m < 58; m++)
+{
+putchar(n);
+putchar(m);
+if (n != 56 || m != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
