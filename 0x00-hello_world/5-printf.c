e24a7688131cacfea344d8380b162abeb50cce86
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-09-10 16:29:35 -0500

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+* File: 5-printf.c
+*
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - A simple program that outputs a line of text using puts()
+* Description: using the main function
+* Return: 0
+*/
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
