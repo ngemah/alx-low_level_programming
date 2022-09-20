1c80f66aa1fdaadbe3bedbc5dac4c896982b9882
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-09-19 21:12:04 -0500

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * _strlen - give the length of a string
- * @s: the string
- *
- * Return: the length of a string
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
  */
 int _strlen(char *s)
 {
-int i;
-for (i = 0;
-s[i] != '\0';
-i++);
-return (i);
+int len = 0;
+while (*(s + len) != '\0')
+len++;
+return (len);
 }
