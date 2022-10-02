22cffb9d2ca4a7ecb3d71e10e585a6f22767994f
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-10-02 17:58:11 -0500

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@ include <stdio.h>
  */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
-(void)argc;
+argc = 0;
+printf("%s\n", argv[argc]);
 return (0);
 }
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+
+*File: main.h
+*Auth: Richard Cudjoe
+
+*Desc: Header file containing prototypes for all functions
+
+* Used for static libraries, how they work, how to create one, and how to use them
+*/
+
+int _putchar(char c);
+int check_num(char *str);
+
+#endif /*MAIN_H*/
