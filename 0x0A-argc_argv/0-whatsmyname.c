14d4575f9b50cf34d2a0a658717979b24ce73536
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-10-02 18:03:31 -0500

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,15 @@
 
 /**
  * main - prints the name of the program
- * 
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-argc = 0;
-printf("%s\n", argv[argc]);
+(void) argc;
+printf("%s\n", argv[0]);
 return (0);
 }
