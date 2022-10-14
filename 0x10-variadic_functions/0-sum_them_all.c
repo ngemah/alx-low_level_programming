19208436c4119673e3d5ec0c78e49f391e1a3faf
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-10-14 18:05:48 -0500

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,22 +2,19 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - sums all arguments passed
- *
- * @n: number of arguments
- *
- * Return: sum of arguments
+ * sum_them_all - returns the sum of all of its parameter
+ * @n: amount of the argument
+ * Return: sum of its parameter
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 va_list valist;
 unsigned int i;
 int sum = 0;
-if (n==0)
+if (n == 0)
 return (0);
 va_start(valist, n);
-for (i = 0, i < n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(valist, int);
 va_end(valist);
 return (sum);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,36 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS.H
-#define VARIADIC_FUNCTIONS.H
-#include <stdarg.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
-
-/**
-
-*File: variadic_functions.h
-*Auth: Richard Cudjoe
-
-*Desc: Header file containing prototypes for all functions
-
-* Used for Variadic functions
-*/
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-
-
-/**
- * struct funckey - a key for picking functions to use for printing
- * in 3-print_all.c
- *
- * @f: function to use for printing
- * @spec: character specifier
- */
-typedef struct funckey
-{
-void (*f)(va_list);
-char spec;
-} funckey;
 
 #endif
