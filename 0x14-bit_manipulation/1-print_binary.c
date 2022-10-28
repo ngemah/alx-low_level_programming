50b4c1ecde4f70c32e9b31e7bbfdf2c6d0740ba0
Richard Cudjoe <richard.cudjoe@aiesec.net>
2022-10-27 20:39:32 -0500

    1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,34 +7,9 @@
 
 void print_binary(unsigned long int n)
 {
-signed long int size;
-char c;
-int flag;
-size = sizeof(n) * 8 - 1;
-if (n == 0)
+if (n > 1)
 {
-printf("0");
-return;
+print_binary(n >> 1);
 }
-if (n == 1)
-{
-printf("1");
-return;
-}
-flag = 0;
-while (size >= 0)
-{
-c = (n >> size) & 1;
-if (flag == 1)
-putchar(c + '0');
-else
-{
-if (c == 1)
-{
-putchar(c + '0');
-flag = 1;
-}
-}
-size -= 1;
-}
+_putchar((n & 1) + '0');
 }
